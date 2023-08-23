6da38ab811fdcb2afb2681861ad586325bf960c7
OladulllEK <137177056+pydragon1@users.noreply.github.com>
2023-08-23 22:59:00 +0700

    Add files via upload

---
+++ PasswordGenerator/main.py
@@ -0,0 +1,27 @@
+import random
+import json
+
+
+with open('settings.json') as setting:
+    settings_data = json.load(fp=setting)
+    
+"""settings.json is required to configure password generation"""
+
+# numbers_pieces - number of passwords to output
+# length - password length
+# characters - characters that will be used when generating a password
+
+class PassGen:
+    def generate(self,
+                 numbers_pieces=settings_data['numbers_pieces'],
+                 length=settings_data['password_length'],
+                 characters=settings_data['characters'] ):
+        
+        for _ in range(numbers_pieces):
+            
+            yield ''.join(random.choices(characters, k=length))
+
+password_generator = PassGen()
+passwords = password_generator.generate()
+for password in passwords:
+    print(password)
