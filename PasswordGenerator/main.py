22f440edd7a61a1ad4cb01b609c260d6290bc40d
OladulllEK <137177056+pydragon1@users.noreply.github.com>
2023-08-27 20:14:19 +0700

    Update main.py

--- PasswordGenerator/main.py
+++ PasswordGenerator/main.py
@@ -1,28 +1,50 @@
-from random import choices
+import customtkinter as ct
+import random
 import json
 
 
+app = ct.CTk()
 
-    
-"""settings.json is required to configure password generation"""
 
-# numbers_pieces - number of passwords to output
-# length - password length
-# characters - characters that will be used when generating a password
+with open('settings.json') as setting:
+    settings_data = json.load(fp=setting)
 
-class PassGen:
-    def generate(self,
-                 numbers_pieces=settings_data['numbers_pieces']: int,
-                 length=settings_data['password_length']: int,
-                 characters=settings_data['characters']: str) -> str:
-        with open('settings.json') as setting:
-            settings_data = json.load(fp=setting)
-        
-        for _ in range(numbers_pieces):
-            
-            yield ''.join(choices(characters, k=length))
 
-password_generator = PassGen()
-passwords = password_generator.generate()
-for password in passwords:
-    print(password)
+def slider_command(value):
+    label_info_length.configure(text=int(value))
+
+
+def generate_password():
+    length = int(slider.get())
+    characters = settings_data['characters']
+    password = ''.join(random.choices(characters, k=length))
+    label_password.configure(text=password)
+
+
+app.geometry('470x270')
+app.resizable(False, False)
+app.title("Password generator")
+
+
+slider_frame = ct.CTkFrame(app, width=460, height=50)
+slider_frame.pack(side='bottom' ,pady=5)
+slider_label = ct.CTkLabel(slider_frame, text='Password Length:  ', font=('Arial', 12))
+slider_label.pack(side='left')
+slider = ct.CTkSlider(slider_frame,command=slider_command, from_=1, to=settings_data['max_password_length'])
+slider.pack(side='left')
+
+
+label_info_length = ct.CTkLabel(slider_frame, text='20')
+label_info_length.pack(side='left', pady=20)
+
+
+btn_generate = ct.CTkButton(app, text='Generate', width=140, height=40, command=generate_password)
+btn_generate.pack(side='bottom', pady=15)
+
+
+pass_frame = ct.CTkFrame(app, width=460, height=100)
+pass_frame.pack(anchor='n', pady=5)
+label_password = ct.CTkLabel(pass_frame, text='hello', width=460, height=100, font=('Arial', 18))
+label_password.pack()
+
+app.mainloop()
